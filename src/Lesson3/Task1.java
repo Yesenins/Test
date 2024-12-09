f411d420d6441bf1ef92299b552dcb80905b1e10
Sergeant Svyatskiy <sithserega@gmail.com>
2024-12-09 19:38:51 +0300

    new branch

--- src/Lesson3/Task1.java
+++ src/Lesson3/Task1.java
@@ -2,6 +2,6 @@ package Lesson3;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Будем мержить");
+        System.out.println("let's merge");
     }
 }
